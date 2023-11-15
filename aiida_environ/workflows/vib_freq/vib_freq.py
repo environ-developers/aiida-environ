4d69d9fda4c94e536f8432591840fbb3c15c536c
wengmouyi <mouyi.weng@epfl.ch>
2023-11-15 11:25:36 +0000

    high order derivative calculation in freq

--- aiida_environ/workflows/vib_freq/vib_freq.py
+++ aiida_environ/workflows/vib_freq/vib_freq.py
@@ -5,6 +5,7 @@ from aiida_environ.calculations.pw import EnvPwCalculation
 from aiida.common.extendeddicts import AttributeDict
 import aiida
 from aiida import orm
+import time
 import numpy as np
 
 EnvCalculation = CalculationFactory('environ.pw')
@@ -28,6 +29,8 @@ class EnvVibfreqWorkchain(WorkChain):
                    help='atoms that can move.')
         spec.input('move_dir', valid_type=orm.List,
                    help='directions that atoms can move.', default=lambda: orm.List([1,1,1]))
+        spec.input('order', valid_type=orm.Int,
+                   help='Orders to calculate force constant, first derivative of force.', default=lambda: orm.Int(2))
         spec.output_namespace(
             'supercells_forces', valid_type=(orm.ArrayData, orm.TrajectoryData), required=True,
             help='The forces acting on the atoms of each supercell.'
@@ -46,11 +49,12 @@ class EnvVibfreqWorkchain(WorkChain):
             cls.cal_freq,
         )
         spec.exit_code(401, 'ERROR_ENVIRON',
-                       message='The environ  process failed')
+                       message='The environ process failed')
 
     
     def setup(self):
-    
+        if (self.inputs.order.value> 4):
+            return self.exit_codes.ERROR_TOO_LARGE_ORDER
         self.ctx.structure = self.inputs.structure
         
     def run_basic_scf(self):
@@ -74,7 +78,7 @@ class EnvVibfreqWorkchain(WorkChain):
             return self.exit_codes.ERROR_FAILED_BASE_SCF
     
     def run_vib(self):
-        structures = self.generate_from_structure(self.inputs.structure, self.inputs.step, self.inputs.move_atoms.get_list(), self.inputs.move_dir.get_list())
+        structures = self.generate_from_structure(self.inputs.structure, self.inputs.step, self.inputs.move_atoms.get_list(), self.inputs.move_dir.get_list(), self.inputs.order.value)
         count = 0
         calc=[]
         base_key = f'{self._RUN_PREFIX}_0'
@@ -84,7 +88,7 @@ class EnvVibfreqWorkchain(WorkChain):
             
             inputs = AttributeDict(self.exposed_inputs(EnvPwCalculation, namespace='environ'))
             inputs.structure = i_structure
-            key_i, key_j, key_k = self.get_number(count, self.inputs.move_atoms.get_list(), self.inputs.move_dir.get_list())
+            key_i, key_j, key_k = self.get_number(count, self.inputs.move_atoms.get_list(), self.inputs.move_dir.get_list(), self.inputs.order.value)
             # key = f'{self._RUN_PREFIX}_{int((count)/12)}_{int((count)%12/4)}_{(count)%4}'
             key = f'{self._RUN_PREFIX}_{key_i}_{key_j}_{key_k}'
             inputs.metadata.label = key
@@ -101,6 +105,7 @@ class EnvVibfreqWorkchain(WorkChain):
             count = count+1
             self.to_context(**{key: calc[count-1]})
             self.report(f'submitting `PhonopyCalculation{key}` <PK={calc[count-1].pk}>')
+            time.sleep(2)
         
             
     def inspect_vib_calculations(self):
@@ -138,11 +143,11 @@ class EnvVibfreqWorkchain(WorkChain):
                 if (self.inputs.move_dir.get_list()[i_dir]==0):
                     continue
                 force_temp = []
-                for i_move in range(5):
-                    if (i_move==2):
+                for i_move in range(2*self.inputs.order.value+1):
+                    if (i_move==self.inputs.order.value):
                         force_temp.append(force_0)
                     else:
-                        if (i_move<2):
+                        if (i_move<self.inputs.order.value):
                             temp_i_move=i_move
                         else:
                             temp_i_move=i_move-1
@@ -152,7 +157,7 @@ class EnvVibfreqWorkchain(WorkChain):
                     if info['symbol'] == self.inputs.structure.sites[i_atom].kind_name:
                         mass_temp = info['mass'] 
                         break
-                force_constant_temp = self.cal_first_deriv(force_temp, mass_temp)
+                force_constant_temp = self.cal_first_deriv(force_temp, mass_temp, self.inputs.order.value)
                 force_constant.append(force_constant_temp)
         
         c1=[]
@@ -177,12 +182,12 @@ class EnvVibfreqWorkchain(WorkChain):
         self.out('freq',temp_freq)
         
         
-    def generate_from_structure(self, structure, step, atoms, dir):
+    def generate_from_structure(self, structure, step, atoms, dir, order):
         structure_list =[]
         for i_atom in range(len(atoms)):
             for i_dir in range(3):
-                for i_move in range(5):
-                    if (i_move==2):
+                for i_move in range(order*2+1):
+                    if (i_move==order):
                         continue
                     cell = []
                     for i in range(3):
@@ -194,20 +199,25 @@ class EnvVibfreqWorkchain(WorkChain):
                         if i == atoms[i_atom]:
                             move_dir=[0,0,0]
                             move_dir[i_dir]=1
-                            p=(structure.sites[i].position[0]+move_dir[0]*step.value*(i_move-2),
-                               structure.sites[i].position[1]+move_dir[1]*step.value*(i_move-2), 
-                               structure.sites[i].position[2]+move_dir[2]*step.value*(i_move-2))
+                            p=(structure.sites[i].position[0]+move_dir[0]*step.value*(i_move-order),
+                               structure.sites[i].position[1]+move_dir[1]*step.value*(i_move-order), 
+                               structure.sites[i].position[2]+move_dir[2]*step.value*(i_move-order))
                         else:
                             p=(structure.sites[i].position[0], structure.sites[i].position[1], structure.sites[i].position[2])
                         s.append_atom(position=p, symbols=structure.sites[i].kind_name)
                     if (dir[i_dir]==1):
                         structure_list.append(s)
-        print(len(structure_list))
+        # print(len(structure_list))
         return structure_list
     
     
-    def cal_first_deriv(self, force, mass):
+    def cal_first_deriv(self, force, mass, order):
         force_constant=[]
+        first_deriv_coeff=[]
+        first_deriv_coeff.append([-0.5, 0.5])
+        first_deriv_coeff.append([1.0/12.0, -2.0/3.0, 2.0/3.0, -1.0/12.0])
+        first_deriv_coeff.append([-1.0/60.0, 3.0/20.0, -3.0/4.0, 3.0/4.0, -3.0/20.0, 1.0/60.0])
+        first_deriv_coeff.append([1.0/280.0, -4.0/105.0, 1.0/5.0, -4.0/5.0, 4.0/5.0, -1.0/5.0, 4.0/105.0, -1.0/280.0])
         for i_atom in self.inputs.move_atoms.get_list(): 
             for key, info in aiida.common.constants.elements.items():
                 if info['symbol'] == self.inputs.structure.sites[i_atom].kind_name:
@@ -217,28 +227,35 @@ class EnvVibfreqWorkchain(WorkChain):
                 if self.inputs.move_dir.get_list()[i_dir]==0:
                     continue
                 delta_f=[]
-                delta_f.append((force[0][i_atom][i_dir]-force[2][i_atom][i_dir])/np.sqrt(mass_temp)/np.sqrt(mass))
-                delta_f.append((force[1][i_atom][i_dir]-force[2][i_atom][i_dir])/np.sqrt(mass_temp)/np.sqrt(mass))
-                delta_f.append((force[3][i_atom][i_dir]-force[2][i_atom][i_dir])/np.sqrt(mass_temp)/np.sqrt(mass))
-                delta_f.append((force[4][i_atom][i_dir]-force[2][i_atom][i_dir])/np.sqrt(mass_temp)/np.sqrt(mass))
-                force_constant.append(-(delta_f[0]-8*delta_f[1]+8*delta_f[2]-delta_f[3])/12.0/self.inputs.step.value)
+                for i_move in range(order*2):
+                    if (i_move<order):
+                        temp_move_i=i_move
+                    else:
+                        temp_move_i=i_move+1
+                    delta_f.append((force[temp_move_i][i_atom][i_dir]-force[order][i_atom][i_dir])/np.sqrt(mass_temp)/np.sqrt(mass))
+                if (len(delta_f)!=len(first_deriv_coeff[order-1])):
+                    return self.exit_codes.ERROR_WRONG_FIRST_DERIVATIVE
+                sum_deriv=0
+                for i_derv in range(len(delta_f)):
+                    sum_deriv=sum_deriv+delta_f[i_derv]*first_deriv_coeff[order-1][i_derv]
+                force_constant.append(-sum_deriv/self.inputs.step.value)
         return force_constant
     
-    def get_number(self, count, atom_list, dir_list):
+    def get_number(self, count, atom_list, dir_list, order):
         sum_dir=0
         for i in range(len(dir_list)):
             sum_dir=sum_dir+dir_list[i]
-        set_number = int(sum_dir*4)
+        set_number = int(sum_dir*(order*2))
         atom_number = atom_list[int(count/set_number)]
-        dir_number = int((count)%set_number/4)
-        sum_dir=0
+        dir_number = int((count)%set_number/(order*2))
+        sum_dir=-1
         for i in range(len(dir_list)):
+            sum_dir=sum_dir+dir_list[i]
             if (sum_dir==dir_number):
                 break
-            sum_dir=sum_dir+1
         direction_label=['x','y','z']
         output_dir = direction_label[i]
-        return atom_number, output_dir, count%4
+        return atom_number, output_dir, count%(order*2)
 
 
     
